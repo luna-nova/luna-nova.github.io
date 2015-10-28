31dc3f3c57b4cf15f3228a9343ab9640fce0ed14
luna-nova <heartofnova@gmail.com>
2015-10-28 15:23:09 -0700

    picture border color changes

--- js/action.js
+++ js/action.js
@@ -1,9 +1,26 @@
 $(document).on("ready", function() {
-  $(".icon").hover(
-    function() {
-      $(this).next().slideDown("fast");
-    },
-    function() {
-      $(this).next().slideUp("fast");
+
+  var randomHue = function() {
+    return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  };
+
+  function gayRainbow() {
+    $(this).animate({ borderColor: randomHue() }, 'slow', function() {
+    $(this).animate({ borderColor: randomHue() }, 'slow', gayRainbow);
     });
+  }
+
+  $("#profile-avatar").hover(gayRainbow, function(){
+    $(this).stop();
+  });
+
+  $(".icon").hover(
+  function() {
+    $(this).next().slideDown("fast");
+  },
+  function() {
+    $(this).next().slideUp("fast");
+  });
+
+
 })

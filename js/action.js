75a419a764ff01795e38e815af787493f35548b8
luna-nova <heartofnova@gmail.com>
2015-10-28 17:21:31 -0700

    starting commenting code

--- js/action.js
+++ js/action.js
@@ -1,16 +1,17 @@
 $(document).on("ready", function() {
 
+  //Selecting a random hue for color changing functions
   var randomHue = function() {
     return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   };
-
-  function gayRainbow() {
+  //Random color changing function for border #supergay
+  function gayRainbowBorder() {
     $(this).animate({ borderColor: randomHue() }, 'slow', function() {
-    $(this).animate({ borderColor: randomHue() }, 'slow', gayRainbow);
+    $(this).animate({ borderColor: randomHue() }, 'slow', gayRainbowBorder);
     });
   }
-
-  $("#profile-avatar").hover(gayRainbow, function(){
+  
+  $("#profile-avatar").hover(gayRainbowBorder, function(){
     $(this).stop();
   });
 
